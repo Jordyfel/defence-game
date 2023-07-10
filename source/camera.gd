a8f7ab0a74af0dc5f7c696f35d892a3fbe3faf33
Veli82 <hearthstoner@abv.bg>
2023-07-10 15:04:00 +0300

    Work on camera.

--- source/camera.gd
+++ source/camera.gd
@@ -1,11 +1,16 @@
 extends Camera3D
+@onready var camera_3d: Camera3D = $"."
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("move_camera_up"):
+		camera_3d.position.z += -1
+		#fix that
+		#use delta nd vectors and stuff
+		#mby use other project for reference /or better just google
